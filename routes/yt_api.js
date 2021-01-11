d517904dd87abe499756068b88652aa3d199b338
Nicolás Montenegro <niko_nmv@live.cl>
2021-01-11 15:52:58 -0300

    Json Schema for query

--- routes/yt_api.js
+++ routes/yt_api.js
@@ -1,21 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+var Validator = require('jsonschema').Validator;
+var validator = new Validator();
+var schema = {
+  "id": "/VideoSearch",
+  "type": "object",
+  "properties": {
+    "searchQuery": {
+      "description": "Text of terms for search",
+      "type": "String"
+    },
+    "pageToken": {
+      "description": "Token to navigate to a specific page",
+      "type": "String"
+    },
+  },
+  "required": ["searchQuery"]
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(req.query);
-  req.app.locals.youtube.search.list({    
-    part: 'snippet',
-    type: 'video',
-    q: req.query.search_query,
-    maxResults: 10
-    }).then(yt_res => {
-      res.send(yt_res.data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
-  
+  const validation =vvalidator.validate(req.query, schema)
+  if (validation.errors.length) {
+    next(validation.errors[0]) // Pass errors to Express.
+  } else {
+    req.app.locals.youtube.search.list({    
+      part: 'snippet',
+      type: 'video',
+      q: req.query.searchQuery,
+      pageToken: req.query.pageToken || null,
+      maxResults: 10
+      }).then(yt_res => {
+        res.json(yt_res.data);
+      })
+      .catch(error => {
+        next(error);
+      });  
+  }
+
+
 });
 
 module.exports = router;
